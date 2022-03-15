39873b1f9c0b9427bd9a4886c43c5eea99d31755
huanmingchang <huanming1988@gmail.com>
2022-03-16 07:50:13 +0800

    feat: add vue data

--- src/components/SubCommercial.vue
+++ src/components/SubCommercial.vue
@@ -5,33 +5,51 @@
     <div
       class="text-[#222222] font-semibold text-3xl mb-10 md:text-4xl md:max-w-[404px] lg:mb-0"
     >
-      Your Dream Destination: Be who you want to be
+      {{ title }}
     </div>
     <div
       class="md:flex md:flex-row md:justify-between md:w-full lg:max-w-[717px]"
     >
-      <div class="md:w-1/2 md:mr-4 lg:max-w-[313px]">
+      <div
+        class="md:w-1/2 md:mr-4 lg:max-w-[313px]"
+        v-for="commercial in commercials"
+        :key="commercial.id"
+      >
         <div
           class="text-[#333333] text-2xl font-semibold mb-4 md:max-w-[313px]"
         >
-          Metropolitan
+          {{ commercial.subtitle }}
         </div>
         <div class="text-[#484848] text-base md:max-w-[313px]">
-          One of the most diverse city in the world. Everything you need, find
-          it here!
-        </div>
-      </div>
-      <div class="md:w-1/2 lg:max-w-[313px]">
-        <div
-          class="text-[#333333] text-2xl font-semibold mb-4 md:max-w-[313px]"
-        >
-          Isolate Islands
-        </div>
-        <div class="text-[#484848] md:max-w-[313px]">
-          Get tired of busy life? Countless isolate islands for you escaping
-          from reality.
+          {{ commercial.text }}
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import { ref, reactive } from 'vue'
+export default {
+  setup() {
+    const title = ref('Your Dream Destination: Be who you want to be')
+    const commercials = reactive([
+      {
+        id: 1,
+        subtitle: 'Metropolitan',
+        text: 'One of the most diverse city in the world. Everything you need, find it here!',
+      },
+      {
+        id: 2,
+        subtitle: 'Isolate Islands',
+        text: 'Get tired of busy life? Countless isolate islands for you escaping from reality.',
+      },
+    ])
+
+    return {
+      title,
+      commercials,
+    }
+  },
+}
+</script>
