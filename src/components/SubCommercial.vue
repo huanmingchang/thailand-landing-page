2e239551093226b53dfb29fcba7aece948c90198
huanmingchang <huanming1988@gmail.com>
2022-03-16 07:32:09 +0800

    feat: finish subCommercial html and css styles

---
+++ src/components/SubCommercial.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="my-6 px-6 max-w-[313px] md:flex md:flex-col md:max-w-[100vw] md:my-8 md:px-10 lg:flex-row lg:items-center lg:justify-between lg:max-w-[1280px] lg:my-12 lg:px-20 lg:mx-auto"
+  >
+    <div
+      class="text-[#222222] font-semibold text-3xl mb-10 md:text-4xl md:max-w-[404px] lg:mb-0"
+    >
+      Your Dream Destination: Be who you want to be
+    </div>
+    <div
+      class="md:flex md:flex-row md:justify-between md:w-full lg:max-w-[717px]"
+    >
+      <div class="md:w-1/2 md:mr-4 lg:max-w-[313px]">
+        <div
+          class="text-[#333333] text-2xl font-semibold mb-4 md:max-w-[313px]"
+        >
+          Metropolitan
+        </div>
+        <div class="text-[#484848] text-base md:max-w-[313px]">
+          One of the most diverse city in the world. Everything you need, find
+          it here!
+        </div>
+      </div>
+      <div class="md:w-1/2 lg:max-w-[313px]">
+        <div
+          class="text-[#333333] text-2xl font-semibold mb-4 md:max-w-[313px]"
+        >
+          Isolate Islands
+        </div>
+        <div class="text-[#484848] md:max-w-[313px]">
+          Get tired of busy life? Countless isolate islands for you escaping
+          from reality.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
