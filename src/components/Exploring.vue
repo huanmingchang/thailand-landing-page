2cfcf3a7b6602a4fcef53cd42a879a645c5154dc
huanmingchang <huanming1988@gmail.com>
2022-03-20 09:27:21 +0800

    fix: update Exploring layout and change bg-img

--- src/components/Exploring.vue
+++ src/components/Exploring.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
+  <div
+    class="max-w-[1280px] h-[544px] my-6 mx-0 mobile:h-[684px] pad:h-[464px] pad:my-8 pad:mx-10 pad:rounded-2xl desktop:my-12 desktop:mx-20 max:mx-auto"
+  >
     <div
-      class="max-w-[1280px] flex flex-col justify-evenly h-[544px] px-6 mb-6 mobile:h-[684px] pad:h-[464px] pad:px-10 pad:mx-10 pad:rounded-2xl max:mx-20 bg-[url('/public/IMG_1571.JPG')] bg-no-repeat bg-cover"
+      class="flex flex-col justify-evenly w-full h-full pad:rounded-2xl bg-[url('/public/IMG_0289.jpeg')] bg-no-repeat bg-cover"
     >
-      <div class="text-xs font-bold text-[#fff] pad:text-lg">
+      <div class="text-xs font-bold text-[#fff] px-6 pad:text-lg">
         {{ slogan }}
       </div>
       <div
-        class="text-3xl font-bold text-[#fff] leading-[42px] mobile:max-w-[258px] pad:max-w-[466px] pad:text-[42px] pad:leading-[54.8px]"
+        class="text-3xl font-bold text-[#fff] px-6 leading-[42px] mobile:max-w-[258px] pad:max-w-[466px] pad:text-[42px] pad:leading-[54.8px]"
       >
         {{ mainTitle }}
       </div>
-      <div class="h-14">
+      <div class="h-14 px-6">
         <a :href="button.link" target="_blank" class="w-[90px] h-full">
           <button
             class="btn h-full py-4 px-8 rounded-lg bg-[#E61E4D] text-[#fff] cursor-pointer"
